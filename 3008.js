9049332469cf74c16eb02bd7b19dbf06568168b1
Cristian Douglas Alves <cristian_alves07@hotmail.com>
2024-09-11 11:11:09 -0300

    Add files via upload

---
+++ 3008.js
@@ -0,0 +1,19 @@
+var setadireita = window.document.getElementById("setadireita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+
+
+function RolarParaDireita(){
+Leonardo.style ="display:none"
+Bruna.style ="display:flex"
+setadireita.style="display:none"
+setaesquerda.style="display:flex; margin-top:55px "}
+
+function RolarParaEsquerda(){
+    Leonardo.style ="display:flex"
+    Bruna.style ="display:none"
+    setadireita.style="display:none; margin-top:55px"}
+    
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# projeto-landing-page
-Este é o projeto de desenvolvimento base do curso de tecnologia da Escola DNC
+# 1109landing
+Disponível aqui no github
